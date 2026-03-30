0a00b890b682ce84a1d79ca364d8b875d88ac7d1
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-03-30 04:53:41 -0400

    Implement service worker for asset caching
    This service worker caches specified assets during installation and cleans up old caches during activation.

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'Omnis-Kernel-018810-v10';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './3pm.mp3'
+];
+
+// Force immediate activation of the new kernel
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Clean out the old dead-Y-axis versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
