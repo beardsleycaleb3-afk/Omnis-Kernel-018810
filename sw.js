2c2dbb2cc12ff45177e58535983c9ba30b31f58d
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-03-30 04:22:49 -0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'Omnis-Kernel-018810-v6';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './3pm.mp3',
+  './Omnis-Kernel.mp3'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
